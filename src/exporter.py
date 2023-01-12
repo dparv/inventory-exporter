ff5e98cb6a323705ac6a646924dba21f9455a850
Diko Parvanov <diko.parvanov@canonical.com>
2023-01-12 16:08:12 +0200

    Updated code to properly use dpkg and added kernel

--- src/exporter.py
+++ src/exporter.py
@@ -11,7 +11,7 @@ class Exporter(BaseHTTPRequestHandler):
 
     def _set_response(self):
         self.send_response(200)
-        if self.path == '/apt' or self.path == '/snap':
+        if self.path in ['/apt', '/snap', '/kernel']:
             self.send_header('Content-type', 'application/json')
         else:
             self.send_header('Content-type', 'text/html')
@@ -25,6 +25,8 @@ class Exporter(BaseHTTPRequestHandler):
             self.wfile.write(self.generate_apt_output().encode("utf-8"))
         elif self.path ==  '/snap':
             self.wfile.write(self.generate_snap_output().encode("utf-8"))
+        elif self.path ==  '/kernel':
+            self.wfile.write(self.generate_kernel_output().encode("utf-8"))
         else:
             helper= """
             <a href='/hostname'>/hostname</a>
@@ -32,25 +34,27 @@ class Exporter(BaseHTTPRequestHandler):
             <a href='/apt'>/apt</a>
             <br />
             <a href='/snap'>/snap</a>
+            <br />
+            <a href='/kernel'>/kernel</a>
             """
             self.wfile.write(helper.encode("utf-8"))
 
     def generate_apt_output(self):
-        cmd = 'dpkg -l'
+        cmd = 'dpkg -l --admindir=/var/lib/snapd/hostfs/var/lib/dpkg'
         apts = subprocess.check_output(cmd.split())
         apts = str(apts)
         lines = apts.split('\\n')
         lines = lines[5:-1]
-        export = []
+        output = []
         for line in lines:
             items = line.split()
             package = items[1]
             version = items[2]
-            export.append({
+            output.append({
                 'package': package,
                 'version': version,
                 })
-        return json.dumps(export)
+        return json.dumps(output)
 
     def generate_snap_output(self):
         cmd = 'snap list'
@@ -58,21 +62,27 @@ class Exporter(BaseHTTPRequestHandler):
         snaps = str(snaps)
         lines = snaps.split('\\n')
         lines = lines[1:-1]
-        export = []
+        output = []
         for line in lines:
             items = line.split()
             snap = items[0]
             version = items[1]
             revision = items[2]
             tracking = items[3]
-            export.append({
+            output.append({
                 'snap': snap,
                 'version': version,
                 'revision': revision,
                 'tracking': tracking,
                 })
-        return json.dumps(export)
+        return json.dumps(output)
 
+    def generate_kernel_output(self):
+        cmd = 'uname -r'
+        kernel = subprocess.check_output(cmd.split())
+        kernel = kernel.splitlines()[0]
+        output = {'kernel': str(kernel.decode())}
+        return json.dumps(output)
 
 def main():
     webServer = HTTPServer((hostname, port), Exporter)
